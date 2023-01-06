60576e472d6acadc402e6ac16adef6a7a9d7add6
Kortney Canyon <kcanyon_2000@yahoo.com>
2023-01-06 09:49:30 -0600

    middleware installed

--- api/routines.js
+++ api/routines.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const { getAllRoutines, createRoutine } = require('../db/routines');
 const router = express.Router();
 
 // GET /api/routines
-
+// router.get('/', async (req, res, next) => {
+//     try {
+//         const routines = await getAllRoutines();
+//         res.send(routines);
+//     } catch (error) {
+//         next(error);
+//     }
+// });
 // POST /api/routines
+// router.post('/', requireUser, async (req, res, next) => {
+//     try {
+//         console.log(req.body);
+//         const { routines } = req.body;
 
+//         const newRoutine = await createRoutine({ routines });
+
+//         res.send(newRoutine);
+//     } catch (error) {
+//         next(error);
+//     }
+// });
 // PATCH /api/routines/:routineId
 
 // DELETE /api/routines/:routineId
--- app.js
+++ app.js
@@ -1,7 +1,21 @@
 require("dotenv").config()
 const express = require("express")
 const app = express()
+const cors = require('cors');
+const router = require('./api');
+const morgan = require('morgan');
+const client = require('./db/client');
+// const { application } = require("express");
+client.connect();
+
 
 // Setup your Middleware and API Router here
+client.connect();
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+app.use('/api', router);
+
+
 
 module.exports = app;
