8e376622dafca92cda857b26e8334d9c73186d2a
Kortney Canyon <kcanyon_2000@yahoo.com>
2023-01-06 11:01:03 -0600

    added error handlers in app.js & created .env file

--- api/users.js
+++ api/users.js
@@ -1,101 +1,101 @@
 /* eslint-disable no-useless-catch */
 const express = require("express");
-const router = express.Router();
+const usersRouter = express.Router();
 const jwt = require("jsonwebtoken");
 const { JWT_SECRET } = process.env;
 const { requireUser } = require("./utils");
 const {
-    createUser,
-    getUserByUsername,
-    getPublicRoutinesByUser,
-    getAllRoutinesByUser,
-  } = require("../db");
+  createUser,
+  getUserByUsername,
+  getPublicRoutinesByUser,
+  getAllRoutinesByUser,
+} = require("../db");
 
-// POST /api/users/register
-router.post("/register", async (req, res, next) => {
-    const { username, password } = req.body;
-    try {
-      const _user = await getUserByUsername(username);
-  
-      if (_user) {
-        res.status(401);
-        next({
-          name: "UserTakenError",
-          message: `User ${username} is already taken.`,
-        });
-      } else if (password.length < 8) {
-        res.status(401);
-        next({
-          name: "PasswordTooShortError",
-          message: "Password Too Short!",
-        });
-      } else {
-        const user = await createUser({
-          username,
-          password,
-        });
-  
-        if (!user) {
-          res.status(400);
-          next({
-            name: "UserCreationError",
-            message: "Bad Request",
-          });
-        } else {
-          const token = jwt.sign(
-            {
-              id: user.id,
-              username: user.username,
-            },
-            JWT_SECRET,
-            {
-              expiresIn: "1w",
-            }
-          );
-  
-          res.send({
-            user,
-            message: "thank you for signing up",
-            token,
-          });
-        }
-      }
-    } catch ({ name, message }) {
-      next({ name, message });
-    }
-  });
 
-// POST /api/users/login
-router.post("/login", async (req, res, next) => {
-    const { username, password } = req.body;
-  
-    if (!username || !password) {
-      next({
-        name: "MissingCredentialsError",
-        message: "Please supply both a username and password",
+usersRouter.post("/register", async (req, res, next) => {
+  const { username, password } = req.body;
+  console.log('this is username: ', username);
+  console.log('this is password: ', password);
+
+  try {
+    const _user = await getUserByUsername(username);
+    // console.log("this is underscore user", _user);
+    if (_user) {
+      res.send({
+        error: "Error",
+        message: `User ${_user.username} is already taken.`,
+        name: "UsernameTaken",
       });
     }
-  
-    try {
-      const user = await getUserByUsername(username);
-      const hashedPassword = user.password;
-      const passwordsMatch = await bcrypt.compare(password, hashedPassword);
-      if (passwordsMatch) {
-        const token = jwt.sign(user, JWT_SECRET);
-        res.send({ user, message: "you're logged in!", token: `${token}` });
-      } else {
-        next({
-          name: "IncorrectCredentialsError",
-          message: "Username or password is incorrect",
-        });
-      }
-    } catch (error) {
-      next(error);
+
+    if (password.length < 8) {
+      res.send({
+        error: "Error",
+        message: "Password Too Short!",
+        name: "InsufficientPassword",
+      });
     }
-  });
+
+    // if (password.length >= 8) {
+    const user = await createUser({
+      username,
+      password,
+    });
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username
+      },
+      JWT_SECRET,
+      {
+        expiresIn: "1W",
+      }
+    );
+    console.log("this is user", user);
+    res.send({
+      message: "thank you for signing up",
+      token: token,
+      user: user,
+    });
+    // }
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+// POST /api/users/login
+usersRouter.post("/login", async (req, res, next) => {
+  const { username, password } = req.body;
+
+  if (!username || !password) {
+    next({
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password",
+    });
+  }
+
+  try {
+    const user = await getUserByUsername(username);
+    const hashedPassword = user.password;
+    const passwordsMatch = await bcrypt.compare(password, hashedPassword);
+    if (passwordsMatch) {
+      const token = jwt.sign(user, JWT_SECRET);
+      res.send({ user, message: "you're logged in!", token: `${token}` });
+    } else {
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect",
+      });
+    }
+  } catch (error) {
+    next(error);
+  }
+});
 
 // GET /api/users/me
 
 // GET /api/users/:username/routines
 
-module.exports = router;
+module.exports = usersRouter;
--- app.js
+++ app.js
@@ -10,12 +10,23 @@ client.connect();
 
 
 // Setup your Middleware and API Router here
-client.connect();
+// client.connect();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api', router);
 
+
+app.get('*', (req, res) => {
+    res.status(404).send({ error: '404 - Not Found', message: 'No route found for the requested URL' });
+});
+
+// error handling middleware
+app.use((error, req, res, next) => {
+    console.error('SERVER ERROR: ', error);
+    if (res.statusCode < 400) res.status(500);
+    res.send({ error: error.message, name: error.name, message: error.message, table: error.table });
+});
 
 
 module.exports = app;
