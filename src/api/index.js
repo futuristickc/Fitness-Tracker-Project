51f56810cb88959a045d9aad54d9bdf152a2cef4
David Giwa <david.s.giwa@gmail.com>
2023-01-09 10:57:21 -0600

    FrontEnd work

--- src/api/index.js
+++ src/api/index.js
@@ -5,9 +5,15 @@ const APIURL = `https://fitnesstrac-kr.herokuapp.com/api`;
 
 
 export const registerUser = async (username, password) => {
+<<<<<<< HEAD
   try {
     const response = await fetch(`${APIURL}/users/register`, {
       method: "POST",
+=======
+    try {
+        const response = await fetch(`${APIURL}/users/register`, {
+            method: "POST",
+>>>>>>> 9ce23a7 (FrontEnd work)
       headers: {
         "Content-Type": "application/json",
       },
@@ -22,6 +28,7 @@ export const registerUser = async (username, password) => {
       data: { token },
     } = await response.json();
     return token;
+<<<<<<< HEAD
   } catch (error) {
     console.error(error);
   }
@@ -52,12 +59,45 @@ export const getMe = async (token) => {
   try {
     const response = await fetch(`${APIURL}/users/me`, {
       headers: {
+=======
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const loginUser = async (username, password) => {
+    try {
+        const verify = await fetch(`${APIURL}/users/login`, {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              user: {
+                username,
+                password,
+              },
+            }),
+          });
+          const data = await verify.json();
+          return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export const getMe = async (token) => {
+    try {
+        const response = await fetch(`${APIURL}/users/me`, {
+          headers: {
+>>>>>>> 9ce23a7 (FrontEnd work)
         "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
     });
 
     const { data } = await response.json();
+<<<<<<< HEAD
     return data;
   } catch (error) {
     console.error(error)
@@ -81,3 +121,15 @@ export const fetchRoutines = async () => {
 export const Logout = () => {
   localStorage.clear();
 };
+=======
+    return data;  
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+
+export const Logout = () => {
+    localStorage.clear();
+  };
+>>>>>>> 9ce23a7 (FrontEnd work)
