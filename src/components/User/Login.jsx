e7de9e039ac1ba292536e2a34dc2756f487cf669
Kortney Canyon <kcanyon_2000@yahoo.com>
2023-01-10 14:30:07 -0600

    looking prettier. working so far. updated app.jsx

--- src/components/User/Login.jsx
+++ src/components/User/Login.jsx
@@ -1,27 +1,58 @@
 import React, { useState } from "react";
 import { loginUser } from "../../api";
-import { useNavigate } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 
 const Login = () => {
-    const navigate = useNavigate();
-
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("")
+    const location = useLocation();
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <h1 id="loginpage"> Log Into your account</h1>
-
-            </form>
-        </div>
+        <form className="user" onSubmit={async (e) => {
+            try {
+                e.preventDefault();
+                const token = await loginUser(username, password)
+                setToken(token);
+                localStorage.setItem("token", token);
+                location.pathname = '/';
+                console.log(username, password, token);
+            } catch (error) {
+                console.error(error)
+            }
+        }}>
+            <label>
+                <input
+                    value={username}
+                    type="text"
+                    placeholder="Username"
+                    required
+                    onChange={(e) => setUsername(e.target.value)} />
+            </label>
+            <label>
+                <input
+                    value={password}
+                    type="password"
+                    placeholder="Password"
+                    required
+                    onChange={(e) => setPassword(e.target.value)} />
+            </label>
+            <br />
+            <button type="submit">Log In</button>
+        </form>
     )
+}
+
+export const checkUserLoggedIn = () => {
+    const token = localStorage.getItem('token');
+    return token ? true : false;
 }
 
 
 // const Login = ({}) => {
 //     const [username, setUserName] = useState("");
 //     const [password, setPassword] = useState("");
-  
+
 //     return (
 //       <div>
 //         <form
@@ -59,5 +90,5 @@ const Login = () => {
 //     );
 //   };
 
-  
-  export default Login;
+
+export default Login;
