2243d13f83efd1972e3683725363116fd9c917a1
Kortney Canyon <kcanyon_2000@yahoo.com>
2023-02-07 23:26:44 -0600

    build changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  build: {
+    chunkSizeWarningLimit: 1600,
+  },
 })
